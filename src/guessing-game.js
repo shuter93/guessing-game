3b049a7e0df0f595037e4761a1094c4cf8fe1900
shuter93 <shuter.1993@gmail.com>
2022-08-16 16:45:54 +0300

    feat: add class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,20 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(min, max) {
+        this.setRange(min, max)
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        this.lastGuess = Math.round(((this.max - this.min) / 2) + this.min);
+        return this.lastGuess;
     }
-
     lower() {
-
+        this.setRange(this.min, this.lastGuess);
     }
-
     greater() {
-
+        this.setRange(this.lastGuess, this.max);
     }
 }
 
